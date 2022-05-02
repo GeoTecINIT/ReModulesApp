a4c40df0018c6f84f9cf8a18c6b1390eadc1a430
Claudia Perez <claumpp@hotmail.com>
2022-05-02 10:01:58 +0200

    change token request url

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {GlobalConstants} from '../../shared/GlobalConstants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  private baseUrl = GlobalConstants.backendURL + '/api/user';
   constructor(private http: HttpClient) { }
 
   getToken( uid: string) {
-    return this.http.post('http://localhost:3000/api/user/token',
+    return this.http.post(this.baseUrl + '/token',
       { uid });
   }
 }
