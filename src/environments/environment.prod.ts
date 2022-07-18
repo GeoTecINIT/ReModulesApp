b24740b7e0845a5b127890506d21cc010fb2ae7b
Claudia Perez <claumpp@hotmail.com>
2022-07-18 08:53:45 +0200

    firebase account for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBigxaCwSMHXeYFCDUlNERoxuku05GRRps',
+    authDomain: 'remodulees-eb272.firebaseapp.com',
+    projectId: 'remodulees-eb272',
+    storageBucket: 'remodulees-eb272.appspot.com',
+    messagingSenderId: '389138375181',
+    appId: '1:389138375181:web:dc62b876c7db36a68c2b44',
+    measurementId: 'G-V9EY5Y4EJH'
+  }
 };
